6de4fcfd7f0e1f254bb4794872424da8da1c00f2
Rishabh <rishabhsethiadsce@gmail.com>
2021-02-23 11:35:12 +0530

    Started adding the profile page

--- lib/PostLogin/settings/components/body.dart
+++ lib/PostLogin/settings/components/body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:optymoney/PostLogin/dashboard/dashboarddata.dart';
 
 class Body extends StatefulWidget {
   @override
@@ -8,6 +10,55 @@ class Body extends StatefulWidget {
 class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(""),
+      ),
+      drawer: AppDrawerMain(),
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "Rishabh Sethia",
+                        style: TextStyle(fontWeight: FontWeight.w800),
+                      ),
+                      Text(
+                        "rishabhsethiadsce@gmail.com",
+                        style: TextStyle(fontWeight: FontWeight.w800),
+                      ),
+                    ],
+                  ),
+                  CircleAvatar(
+                    radius: 25,
+                    backgroundColor: Colors.blueGrey,
+                    child: Text(
+                      "R",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Divider(),
+            SizedBox(),
+          ],
+        ),
+      ),
+    );
   }
 }
