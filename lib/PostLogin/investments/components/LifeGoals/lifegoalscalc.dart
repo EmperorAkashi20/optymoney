47d96d62a7d1da549aa0f8479965499b28ca0d5c
Rishabh <rishabhsethiadsce@gmail.com>
2021-03-05 12:04:06 +0530

    Bug Fixes

--- lib/PostLogin/investments/components/LifeGoals/lifegoalscalc.dart
+++ lib/PostLogin/investments/components/LifeGoals/lifegoalscalc.dart
@@ -21,11 +21,22 @@ class _LifeGoalsButtonsState extends State<LifeGoalsButtons> {
                   image: "assets/images/retire.png",
                   title: "Retire Rich",
                   navigationRoute: () {},
+                  imageHeight: 45,
+                  imageWidth: 45,
                 ),
                 LifeGoalsButtonTile(
                   image: "assets/images/car.png",
                   title: "Buy A Car",
                   navigationRoute: () {},
+                  imageHeight: 45,
+                  imageWidth: 45,
+                ),
+                LifeGoalsButtonTile(
+                  image: "assets/images/house.png",
+                  title: "Your House",
+                  navigationRoute: () {},
+                  imageHeight: 45,
+                  imageWidth: 45,
                 ),
               ],
             ),
@@ -36,26 +47,22 @@ class _LifeGoalsButtonsState extends State<LifeGoalsButtons> {
                   image: "assets/images/education.png",
                   title: "Higher Education",
                   navigationRoute: () {},
+                  imageHeight: 45,
+                  imageWidth: 45,
                 ),
-                LifeGoalsButtonTile(
-                  image: "assets/images/house.png",
-                  title: "Own A House",
-                  navigationRoute: () {},
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
                 LifeGoalsButtonTile(
                   image: "assets/images/wedding.png",
                   title: "Grand Wedding",
                   navigationRoute: () {},
+                  imageHeight: 45,
+                  imageWidth: 45,
                 ),
                 LifeGoalsButtonTile(
                   image: "assets/images/vacation.png",
                   title: "Plan A Vacation",
                   navigationRoute: () {},
+                  imageHeight: 45,
+                  imageWidth: 45,
                 ),
               ],
             ),
@@ -66,11 +73,20 @@ class _LifeGoalsButtonsState extends State<LifeGoalsButtons> {
                   image: "assets/images/emergencyfund.png",
                   title: "Emergency Fund",
                   navigationRoute: () {},
+                  imageHeight: 30,
+                  imageWidth: 30,
                 ),
                 LifeGoalsButtonTile(
                   image: "assets/images/uniquegoal.png",
                   title: "Unique Goal",
                   navigationRoute: () {},
+                  imageHeight: 30,
+                  imageWidth: 30,
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                  ),
                 ),
               ],
             ),
@@ -87,11 +103,15 @@ class LifeGoalsButtonTile extends StatelessWidget {
     @required this.image,
     @required this.title,
     @required this.navigationRoute,
+    @required this.imageHeight,
+    @required this.imageWidth,
   }) : super(key: key);
 
   final String image;
   final String title;
   final Function navigationRoute;
+  final double imageHeight;
+  final double imageWidth;
 
   @override
   Widget build(BuildContext context) {
@@ -106,20 +126,21 @@ class LifeGoalsButtonTile extends StatelessWidget {
           ),
           child: Container(
             height: getProportionateScreenHeight(130),
+            width: getProportionateScreenWidth(130),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Image.asset(
                   image,
-                  height: getProportionateScreenHeight(80),
-                  width: getProportionateScreenWidth(80),
+                  height: getProportionateScreenHeight(imageHeight),
+                  width: getProportionateScreenWidth(imageWidth),
                 ),
                 Text(
                   title,
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     color: kPrimaryColor,
-                    fontSize: 18,
+                    fontSize: 13,
                     fontWeight: FontWeight.w700,
                   ),
                 )
