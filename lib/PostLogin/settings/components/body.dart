fa92b5aa81a2b80a86d5d1adc2f092068c8f1d3e
Rishabh <rishabhsethiadsce@gmail.com>
2021-03-09 19:55:08 +0530

    Added Few Details to settings page

--- lib/PostLogin/settings/components/body.dart
+++ lib/PostLogin/settings/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:optymoney/PostLogin/dashboard/dashboarddata.dart';
+import 'package:optymoney/models.dart';
 
 class Body extends StatefulWidget {
   @override
@@ -24,19 +25,22 @@ class _BodyState extends State<Body> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        "Rishabh Sethia",
-                        style: TextStyle(fontWeight: FontWeight.w800),
-                      ),
-                      Text(
-                        "rishabhsethiadsce@gmail.com",
-                        style: TextStyle(fontWeight: FontWeight.w800),
-                      ),
-                    ],
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Rishabh Sethia",
+                          style: TextStyle(fontWeight: FontWeight.w800),
+                        ),
+                        Text(
+                          "work.rishabhsethia@gmail.com",
+                          style: TextStyle(fontWeight: FontWeight.w800),
+                        ),
+                      ],
+                    ),
                   ),
                   CircleAvatar(
                     radius: 25,
@@ -54,8 +58,101 @@ class _BodyState extends State<Body> {
               ),
             ),
             Divider(),
-            SizedBox(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                TitleHeaderForSettings(text: "KYC Compliance"),
+                SettingsPageButton(
+                  text: "Check>>",
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                TitleHeaderForSettings(text: "FAQ & Knowledge Center"),
+                SettingsPageButton(
+                  text: "GO>>",
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                TitleHeaderForSettings(text: "Company Terms"),
+                SettingsPageButton(
+                  text: "GO>>",
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                TitleHeaderForSettings(text: "Privacy Policy"),
+                SettingsPageButton(
+                  text: "GO>>",
+                ),
+              ],
+            ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class SettingsPageButton extends StatelessWidget {
+  const SettingsPageButton({
+    Key key,
+    @required this.text,
+    this.icon,
+  }) : super(key: key);
+
+  final String text;
+  final Icon icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 5.0, right: 5),
+      child: TextButton(
+        style: TextButton.styleFrom(
+          side: BorderSide(color: Colors.red),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(50),
+          ),
+        ),
+        onPressed: () {},
+        child: Center(
+          child: Text(
+            text,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 13, fontWeight: FontWeight.w700, color: Colors.grey),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TitleHeaderForSettings extends StatelessWidget {
+  const TitleHeaderForSettings({
+    Key key,
+    @required this.text,
+  }) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 5.0, left: 20),
+      child: Align(
+        alignment: Alignment.topLeft,
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w900),
         ),
       ),
     );
